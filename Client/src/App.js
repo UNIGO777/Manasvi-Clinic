dbc273d4c68285645d149fb0a47f615f8eca18ce
sajni <sajnikumari30122004@gmail.com>
2024-10-18 15:24:01 +0530

    asbdva

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,11 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import ProtectedRoute from './hoc/ProtectedRoute';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Admin from './portion/admin';
 import Staff from './portion/staff';
 import Patient from './portion/patient';
-import BookAppointment from '../src/components/patient/BookAppointment';
-import Footer from '../src/components/patient/Footer';
-import Appointment from '../src/components/patient/Appointment'; 
+import Appointmentpage from '../src/components/Appointmentpage';
+import Loginpage from '../src/components/Loginpage';
+   
 
 const NotFound = () => {
   return <div className="text-center text-2xl text-red-500">Page Not Found</div>;
@@ -14,21 +13,18 @@ const NotFound = () => {
 
 function App() {
   return (
-    
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/user" />} />
-          <Route path="/admin/*" element={<Admin />} />
-          <Route path="/staff/*" element={<Staff />} />
-          <Route path="/user/*" element={<Patient />} />
-          <Route path="*" element={<NotFound />} />
-          
-          {/* Add more routes as needed */}
-        </Routes>
-         <Appointment/>
-        <BookAppointment/>
-        <Footer/>
-      </Suspense>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/user" />} />
+        <Route path="/admin/*" element={<Admin />} />
+        <Route path="/staff/*" element={<Staff />} />
+        <Route path="/user/*" element={<Patient />} />
+        <Route path="/appointment" element={<Appointmentpage />} />
+        <Route path="/login" element={<Loginpage />} />
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
     
   );
 }
---
+++ Client/src/components/Loginpage.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from  '../components/UserLogin/Login';
+import Footer2 from '../components/UserLogin/Footer2';
+
+
+function Loginpage() {
+    return (
+        <div>
+           
+            <Login />
+            
+          
+        </div>
+    );
+}
+
+export default Loginpage

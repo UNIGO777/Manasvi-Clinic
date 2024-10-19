8893c253b91b64438777a31ac4a51f699b658303
Ayesha Khan <1.ayesha.1.khan1@gmail.com>
2024-10-19 11:22:40 +0530

    The Front End Code__________________________Final

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,20 +3,12 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Admin from './portion/admin';
 import Staff from './portion/staff';
 import Patient from './portion/patient';
-<<<<<<< HEAD
-import BookAppointment from '../src/components/patient/BookAppointment';
-import Footer from '../src/components/patient/Footer';
-import Appointment from '../src/components/patient/Appointment'; 
-import Reports from '../src/portion/patient/pages/Reports';
 import Appointmentpage from '../src/components/Appointmentpage';
 import Loginpage from '../src/components/Loginpage';
 import Review from '../src/components/Review.jsx';
-=======
-import Appointmentpage from '../src/components/Appointmentpage';
-import Loginpage from '../src/components/Loginpage';
-import Review from '../src/components/Review.jsx';
+
    
->>>>>>> 11416f383a8df7850b7eefa48c685bc2e52d0ff4
+
 
 const NotFound = () => {
   return <div className="text-center text-2xl text-red-500">Page Not Found</div>;
@@ -24,11 +16,10 @@ const NotFound = () => {
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <Suspense fallback={<div>Loading...</div>}>
-=======
-    <BrowserRouter>
->>>>>>> 11416f383a8df7850b7eefa48c685bc2e52d0ff4
+    {/* <BrowserRouter> */}
+
       <Routes>
         <Route path="/" element={<Navigate to="/user" />} />
         <Route path="/admin/*" element={<Admin />} />
@@ -41,14 +32,10 @@ function App() {
         {/* <Route path="/reports" element={<Reports />} /> */}
         {/* Add more routes as needed */}
       </Routes>
-<<<<<<< HEAD
     </Suspense>
-=======
-    </BrowserRouter>
 
-    
->>>>>>> 11416f383a8df7850b7eefa48c685bc2e52d0ff4
-  );
+    // </BrowserRouter>
+  )
 }
 
 export default App;

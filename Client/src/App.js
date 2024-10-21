d590762becf8e80a0a44ba2567fb5aabe125462d
sajni <sajnikumari30122004@gmail.com>
2024-10-21 12:29:23 +0530

    responsive commit

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+// import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Admin from './portion/admin';
 import Staff from './portion/staff';
@@ -6,13 +6,9 @@ import Patient from './portion/patient';
 import Appointmentpage from '../src/components/Appointmentpage';
 import Loginpage from '../src/components/Loginpage';
 import Review from '../src/components/Review.jsx';
-<<<<<<< HEAD
-import Reports from '../src/portion/patient/pages/Reports.jsx';
-   
-=======
 import Reports from '../src/portion/patient/pages/Reports';
 
->>>>>>> d4a0e1b842f0a92b616a806d199d93684e22cbec
+
 
 const NotFound = () => {
   return <div className="text-center text-2xl text-red-500">Page Not Found</div>;
@@ -20,34 +16,21 @@ const NotFound = () => {
 
 function App() {
   return (
-
-    <Suspense fallback={<div>Loading...</div>}>
-    {/* <BrowserRouter> */}
-
-      <Routes>
+    // <BrowserRouter> 
+  <Routes>
         <Route path="/" element={<Navigate to="/user" />} />
         <Route path="/admin/*" element={<Admin />} />
         <Route path="/staff/*" element={<Staff />} />
         <Route path="/user/*" element={<Patient />} />
-<<<<<<< HEAD
-        <Route path="/Appointment" element={<Appointmentpage />} />
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/review" element={<Review/>} />
-        <Route path="/reports" element={<Reports/>} />
-        <Route path="*" element={<NotFound />} />
-=======
         <Route path="*" element={<NotFound />} />
         <Route path="/appointment" element={<Appointmentpage />} />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/review" element={<Review />} />
         <Route path="/reports" element={<Reports />} />
         {/* Add more routes as needed */}
->>>>>>> d4a0e1b842f0a92b616a806d199d93684e22cbec
-      </Routes>
-    </Suspense>
-
+     </Routes>  
     // </BrowserRouter>
-  )
+  );
 }
 
 export default App;
